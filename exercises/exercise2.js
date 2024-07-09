f8bc6331b8fab09733b463b0dbf005ae4d676500
saragnzlz <sgonzacu@gmail.com>
2024-07-09 18:04:55 +0200

    Exercises 3 to 9

---
+++ exercises/exercise2.js
@@ -0,0 +1,8 @@
+// Write a function that takes two numbers and  returns the maximum of the two
+
+function getLargestNumber (n1, n2) {
+    return n1 > n2 ? n1 : n2;
+}
+
+console.log(getLargestNumber(10,5))
+console.log(getLargestNumber(8,20))
